18572af05c62dcfb737bcf39e925c3d4cda23a1b
Carlee Miller <carlee.miller@gmail.com>
2023-01-12 21:51:00 -0600

    notes added and starter files

---
+++ Develop/db/store_notes.js
@@ -0,0 +1,1 @@
+// you can use this to access the file system (fs), so you can write, retrieve, delete info from the db.json file
---
+++ Develop/server.js
@@ -0,0 +1,6 @@
+// this is where the page/server will run from
+// require express, fs, path (activity on how to set up express server js #13)
+
+// TODO: you will need to access the file system (fs), so you can write, retrieve, delete info from the db.json file
+
+// need middleware, routes (could use a route folder to separate)
